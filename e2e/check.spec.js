695b6a5afe2788350449e28f1bd140fb941f63c9
Nicolas Molina Monroy <hi@nicobytes.com>
2022-07-14 02:18:42 -0400

    :construction: Fix errors in e2e

--- e2e/check.spec.js
+++ e2e/check.spec.js
@@ -8,7 +8,9 @@ const Schedule = require('../src/database/entities/schedule.entity');
 const initSeedDB = require('../src/database/seeds/init.seed');
 
 const formatSlot = (slot) => {
-  return `${format(slot.startDate, 'HH:mm')}-${format(slot.endDate, 'HH:mm')}`;
+  const startTime = format(new Date(slot.startDate), 'HH:mm');
+  const endTime = format(new Date(slot.endDate), 'HH:mm');
+  return `${startTime}-${endTime}`;
 };
 
 // TODO: E2E DTOS 400 bad request
@@ -32,51 +34,47 @@ describe('Check slots with the same timezone', () => {
       expect(statusCode).toEqual(400);
     });
     test('should return a 404 with invalid date', async () => {
-      const { statusCode } = await api
-        .post('/api/v1/availability')
-        .send({
-          date: 'bla',
-          timezone: 'America/Mexico_City',
-          scheduleId: '12345',
-        });
+      const { statusCode } = await api.post('/api/v1/availability').send({
+        date: 'bla',
+        timezone: 'America/Mexico_City',
+        scheduleId: '12345',
+      });
       expect(statusCode).toEqual(400);
     });
     test('should return a 404 with invalid timezone', async () => {
-      const { statusCode } = await api
-        .post('/api/v1/availability')
-        .send({
-          date: '2022-07-18',
-          timezone: 'bla',
-          scheduleId: '12345',
-        });
+      const { statusCode } = await api.post('/api/v1/availability').send({
+        date: '2022-07-18',
+        timezone: 'bla',
+        scheduleId: '12345',
+      });
       expect(statusCode).toEqual(400);
     });
     test('should return a 404 with invalid scheduleId', async () => {
-      const { statusCode } = await api
-        .post('/api/v1/availability')
-        .send({
-          date: '2022-07-18',
-          timezone: 'America/Mexico_City',
-          scheduleId: '1',
-        });
+      const { statusCode } = await api.post('/api/v1/availability').send({
+        date: '2022-07-18',
+        timezone: 'America/Mexico_City',
+        scheduleId: '1',
+      });
       expect(statusCode).toEqual(400);
     });
   });
 
-  test('should return right slots', async () => {
-    const valeUser = await User.findOne({ email: 'vale@mail.com' });
-    const valeSchedule = await Schedule.findOne({ user: valeUser._id });
+  describe('Generate slots', () => {
+    test('should return right slots', async () => {
+      const valeUser = await User.findOne({ email: 'vale@mail.com' });
+      const valeSchedule = await Schedule.findOne({ user: valeUser._id });
 
-    const data = {
-      date: '2022-07-18',
-      scheduleId: valeSchedule._id,
-      timezone: valeSchedule.timezone,
-    };
-    const { body } = await api.post('/api/v1/availability').send(data);
-    expect(body.length).toEqual(3);
-    expect(formatSlot(body[0])).toEqual('09:00-09:15');
-    expect(formatSlot(body[1])).toEqual('09:20-09:35');
-    expect(formatSlot(body[2])).toEqual('09:40-09:55');
+      const data = {
+        date: '2022-07-18',
+        scheduleId: valeSchedule._id,
+        timezone: valeSchedule.timezone,
+      };
+      const { body } = await api.post('/api/v1/availability').send(data);
+      expect(body.length).toEqual(3);
+      expect(formatSlot(body[0])).toEqual('09:00-09:15');
+      expect(formatSlot(body[1])).toEqual('09:20-09:35');
+      expect(formatSlot(body[2])).toEqual('09:40-09:55');
+    });
   });
 
   afterEach(async () => {
