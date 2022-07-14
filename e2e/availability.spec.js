f83243ece527d753bfea1e1f048ec4e5923fadb9
Nicolas Molina Monroy <hi@nicobytes.com>
2022-07-14 09:45:44 -0400

    :construction: work

--- e2e/availability.spec.js
+++ e2e/availability.spec.js
@@ -1,5 +1,5 @@
 const supertest = require('supertest');
-const { format } = require('date-fns');
+const { utcToZonedTime, format: formatTz  } = require("date-fns-tz");
 
 const createApp = require('../src/app');
 const getConnection = require('../src/database/connection');
@@ -7,10 +7,15 @@ const User = require('../src/database/entities/user.entity');
 const Schedule = require('../src/database/entities/schedule.entity');
 const initSeedDB = require('../src/database/seeds/init.seed');
 
-const formatSlot = (slot) => {
-  const startTime = format(new Date(slot.start), 'HH:mm');
-  const endTime = format(new Date(slot.end), 'HH:mm');
-  return `${startTime}-${endTime}`;
+const formatSlot = (slot, userTZ) => {
+  const startDate = new Date(slot.start);
+  const zonedStartTime = utcToZonedTime(startDate, userTZ);
+  const hourStartTime = formatTz(zonedStartTime, 'HH:mm', {timeZone: userTZ});
+
+  const endDate = new Date(slot.end);
+  const zonedEndTime = utcToZonedTime(endDate, userTZ);
+  const hourEndTime = formatTz(zonedEndTime, 'HH:mm', {timeZone: userTZ});
+  return `${hourStartTime}-${hourEndTime}`;
 };
 
 // TODO: schedule not found
@@ -67,20 +72,24 @@ describe('Tests for availability', () => {
   });
 
   describe('Generating slots', () => {
-    test('should return right slots', async () => {
+    test('should return slots with the same timezone', async () => {
       const valeUser = await User.findOne({ email: 'vale@mail.com' });
       const valeSchedule = await Schedule.findOne({ user: valeUser._id });
+
+      const userTZ = valeSchedule.timezone;
 
       const data = {
         date: '2022-07-18',
         scheduleId: valeSchedule._id,
-        timezone: valeSchedule.timezone,
+        timezone: userTZ,
       };
       const { body } = await api.post('/api/v1/availability').send(data);
       expect(body.length).toEqual(3);
-      expect(formatSlot(body[0])).toEqual('09:00-09:15');
-      expect(formatSlot(body[1])).toEqual('09:20-09:35');
-      expect(formatSlot(body[2])).toEqual('09:40-09:55');
+      expect(valeSchedule.timezone).toEqual('America/Bogota');
+      expect(userTZ).toEqual('America/Bogota');
+      expect(formatSlot(body[0], userTZ)).toEqual('09:00-09:15');
+      expect(formatSlot(body[1], userTZ)).toEqual('09:20-09:35');
+      expect(formatSlot(body[2], userTZ)).toEqual('09:40-09:55');
     });
   });
 
