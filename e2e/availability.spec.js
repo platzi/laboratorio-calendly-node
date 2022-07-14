e8d65cf2783543189a1096f93de2ce1681e8bbf7
Nicolas Molina Monroy <hi@nicobytes.com>
2022-07-14 11:21:18 -0400

    :construction: work

--- e2e/availability.spec.js
+++ e2e/availability.spec.js
@@ -1,5 +1,5 @@
 const supertest = require('supertest');
-const { utcToZonedTime, format: formatTz  } = require("date-fns-tz");
+const { utcToZonedTime, format: formatTz } = require('date-fns-tz');
 
 const createApp = require('../src/app');
 const getConnection = require('../src/database/connection');
@@ -10,11 +10,11 @@ const initSeedDB = require('../src/database/seeds/init.seed');
 const formatSlot = (slot, userTZ) => {
   const startDate = new Date(slot.start);
   const zonedStartTime = utcToZonedTime(startDate, userTZ);
-  const hourStartTime = formatTz(zonedStartTime, 'HH:mm', {timeZone: userTZ});
+  const hourStartTime = formatTz(zonedStartTime, 'HH:mm', { timeZone: userTZ });
 
   const endDate = new Date(slot.end);
   const zonedEndTime = utcToZonedTime(endDate, userTZ);
-  const hourEndTime = formatTz(zonedEndTime, 'HH:mm', {timeZone: userTZ});
+  const hourEndTime = formatTz(zonedEndTime, 'HH:mm', { timeZone: userTZ });
   return `${hourStartTime}-${hourEndTime}`;
 };
 
@@ -71,7 +71,21 @@ describe('Tests for availability', () => {
     });
   });
 
-  describe('Generating slots', () => {
+  describe('Generating slots - 09:00 - 10:00', () => {
+    test('should return empty array', async () => {
+      const valeUser = await User.findOne({ email: 'vale@mail.com' });
+      const valeSchedule = await Schedule.findOne({ user: valeUser._id });
+
+      const userTZ = valeSchedule.timezone;
+
+      const data = {
+        date: '2022-07-19',
+        scheduleId: valeSchedule._id,
+        timezone: userTZ,
+      };
+      const { body } = await api.post('/api/v1/availability').send(data);
+      expect(body.length).toEqual(0);
+    });
     test('should return slots with the same timezone', async () => {
       const valeUser = await User.findOne({ email: 'vale@mail.com' });
       const valeSchedule = await Schedule.findOne({ user: valeUser._id });
@@ -91,7 +105,160 @@ describe('Tests for availability', () => {
       expect(formatSlot(body[1], userTZ)).toEqual('09:20-09:35');
       expect(formatSlot(body[2], userTZ)).toEqual('09:40-09:55');
     });
+    test('should return slots with diff timezone', async () => {
+      const valeUser = await User.findOne({ email: 'vale@mail.com' });
+      const valeSchedule = await Schedule.findOne({ user: valeUser._id });
+
+      const userTZ = 'America/La_Paz';
+
+      const data = {
+        date: '2022-07-18',
+        scheduleId: valeSchedule._id,
+        timezone: userTZ,
+      };
+      const { body } = await api.post('/api/v1/availability').send(data);
+      expect(body.length).toEqual(3);
+      expect(valeSchedule.timezone).toEqual('America/Bogota');
+      expect(userTZ).toEqual('America/La_Paz');
+      expect(formatSlot(body[0], userTZ)).toEqual('10:00-10:15');
+      expect(formatSlot(body[1], userTZ)).toEqual('10:20-10:35');
+      expect(formatSlot(body[2], userTZ)).toEqual('10:40-10:55');
+    });
   });
+
+  describe('Generating slots - 09:00 - 10:15', () => {
+    test('should return empty array', async () => {
+      const santiUser = await User.findOne({ email: 'santi@mail.com' });
+      const santiSchedule = await Schedule.findOne({ user: santiUser._id });
+
+      const userTZ = santiSchedule.timezone;
+
+      const data = {
+        date: '2022-07-19',
+        scheduleId: santiSchedule._id,
+        timezone: userTZ,
+      };
+      const { body } = await api.post('/api/v1/availability').send(data);
+      expect(body.length).toEqual(0);
+    });
+    test('should return slots with the same timezone', async () => {
+      const santiUser = await User.findOne({ email: 'santi@mail.com' });
+      const santiSchedule = await Schedule.findOne({ user: santiUser._id });
+
+      const userTZ = santiSchedule.timezone;
+
+      const data = {
+        date: '2022-07-18',
+        scheduleId: santiSchedule._id,
+        timezone: userTZ,
+      };
+      const { body } = await api.post('/api/v1/availability').send(data);
+      expect(body.length).toEqual(4);
+      expect(santiSchedule.timezone).toEqual('America/Bogota');
+      expect(userTZ).toEqual('America/Bogota');
+      expect(formatSlot(body[0], userTZ)).toEqual('09:00-09:15');
+      expect(formatSlot(body[1], userTZ)).toEqual('09:20-09:35');
+      expect(formatSlot(body[2], userTZ)).toEqual('09:40-09:55');
+      expect(formatSlot(body[3], userTZ)).toEqual('10:00-10:15');
+    });
+    test('should return slots with diff timezone', async () => {
+      const santiUser = await User.findOne({ email: 'santi@mail.com' });
+      const santiSchedule = await Schedule.findOne({ user: santiUser._id });
+
+      const userTZ = 'America/La_Paz';
+
+      const data = {
+        date: '2022-07-18',
+        scheduleId: santiSchedule._id,
+        timezone: userTZ,
+      };
+      const { body } = await api.post('/api/v1/availability').send(data);
+      expect(body.length).toEqual(4);
+      expect(santiSchedule.timezone).toEqual('America/Bogota');
+      expect(userTZ).toEqual('America/La_Paz');
+      expect(formatSlot(body[0], userTZ)).toEqual('10:00-10:15');
+      expect(formatSlot(body[1], userTZ)).toEqual('10:20-10:35');
+      expect(formatSlot(body[2], userTZ)).toEqual('10:40-10:55');
+      expect(formatSlot(body[3], userTZ)).toEqual('11:00-11:15');
+    });
+  });
+
+  describe('Generating slots with multiples intervals', () => {
+
+    test('should return slots with the same timezone', async () => {
+      const nicoUser = await User.findOne({ email: 'nico@mail.com' });
+      const nicoSchedule = await Schedule.findOne({ user: nicoUser._id });
+
+      const userTZ = nicoSchedule.timezone;
+
+      const data = {
+        date: '2022-07-18',
+        scheduleId: nicoSchedule._id,
+        timezone: userTZ,
+      };
+      const { body } = await api.post('/api/v1/availability').send(data);
+      expect(body.length).toEqual(4);
+      expect(nicoSchedule.timezone).toEqual('America/La_Paz');
+      expect(userTZ).toEqual('America/La_Paz');
+      expect(formatSlot(body[0], userTZ)).toEqual('10:00-10:30');
+      expect(formatSlot(body[1], userTZ)).toEqual('10:35-11:05');
+      expect(formatSlot(body[2], userTZ)).toEqual('11:10-11:40');
+      expect(formatSlot(body[3], userTZ)).toEqual('20:00-20:30');
+    });
+    test('should return slots with diffs timezone', async () => {
+      const nicoUser = await User.findOne({ email: 'nico@mail.com' });
+      const nicoSchedule = await Schedule.findOne({ user: nicoUser._id });
+
+      const userTZ = 'America/Bogota';
+
+      const data = {
+        date: '2022-07-18',
+        scheduleId: nicoSchedule._id,
+        timezone: userTZ,
+      };
+      const { body } = await api.post('/api/v1/availability').send(data);
+      expect(body.length).toEqual(4);
+      expect(nicoSchedule.timezone).toEqual('America/La_Paz');
+      expect(userTZ).toEqual('America/Bogota');
+      expect(formatSlot(body[0], userTZ)).toEqual('09:00-09:30');
+      expect(formatSlot(body[1], userTZ)).toEqual('09:35-10:05');
+      expect(formatSlot(body[2], userTZ)).toEqual('10:10-10:40');
+      expect(formatSlot(body[3], userTZ)).toEqual('19:00-19:30');
+    });
+  });
+
+  describe('Booking', () => {
+    test('should return slots with the same timezone', async () => {
+      const nicoUser = await User.findOne({ email: 'nico@mail.com' });
+      const nicoSchedule = await Schedule.findOne({ user: nicoUser._id });
+
+      const userTZ = nicoSchedule.timezone;
+
+      const data = {
+        date: '2022-07-18',
+        scheduleId: nicoSchedule._id,
+        timezone: userTZ,
+      };
+      const response = await api.post('/api/v1/availability').send(data);
+
+      expect(nicoSchedule.timezone).toEqual('America/La_Paz');
+      expect(userTZ).toEqual('America/La_Paz');
+
+      const appointment = response.body[0];
+
+      const responseAppointment = await api.post('/api/v1/appointments').send({
+        userId: nicoUser._id,
+        startDate: new Date(appointment.start).toISOString(),
+        endDate: new Date(appointment.end).toISOString(),
+        note: 'bla bla bla bla bla bla bla',
+        email: 'nico@example.com'
+      });
+      expect(responseAppointment.status).toEqual(200);
+
+      const { body } = await api.post('/api/v1/availability').send(data);
+      expect(body[0].status).toEqual('off');
+    });
+  })
 
   afterEach(async () => {
     server.close();
